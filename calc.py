73fae4ff5a9b8a378b40ce5e229ce2715ec8c93d
sangee17-sangee <sangeetha.m22td0715@svcet.ac.in>
2025-01-31 09:32:35 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,13 @@
+def add(num1, num2):
+    return num1 + num2
+def subtract(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2    
+def divide(num1, num2):
+    return num1 / num2
+def sqaure(x):
+    return(x**2)
+def module(x,y):
+    return(x%y)
+    
---
+++ calimp.py
@@ -0,0 +1,3 @@
+import calc
+print(calc.add(num1,num2))
+print(calc.sub(num1,num2))
